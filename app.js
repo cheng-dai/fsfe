899a8e3d1de6c99e77b73b6ee5684595dcbb1401
Cheng Dai <chengdai.dev@gmail.com>
2025-05-24 11:08:57 +0000

    init

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+http.createServer((req,res) => {
+    res.write("kulturbullar 2025, see you soon.")
+    res.end();
+}
+    
+
+).listen(3000);
+
+console.log('Server started on port 3000')
